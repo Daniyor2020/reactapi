5b7def04bb7733b1a56d4c8addbd2769cf3650ec
Daniyor2020 <dani.bolta87@gmail.com>
2020-07-21 22:01:26 +0800

    nest: report,Service

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,25 +1,36 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ReportsService } from './reports.services';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
   @Get()
-  getAll() {
-    return 'all reports!';
+  getAll(): any {
+    return this.reportsService.findAll();
   }
-  @Get (':id')
-  get(){
-      return 'get report with id!'
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reportsService.find(id);
   }
   @Post()
-  create() {
-      return " create reports!"
+  Create(@Body() report: any) {
+    return this.reportsService.create(report);
   }
-  @Put()
-  update() {
-      return "update report!"
+  @Put(':id')
+  Update(@Param('id') id: string, @Body() report: any) {
+    return this.reportsService.update(id, report);
   }
-  @Delete()
-  delete() {
-      return 'delete report!'
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.reportsService.delete(id);
   }
 }
--- src/reports/reports.services.ts
+++ src/reports/reports.services.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello React BIY!';
+export class ReportsService {
+
+  delete(id: string):any{
+    this.data[this.data.findIndex(item=>item.id==id)]
+    return `removed report by id ${id}`
+    
   }
+
+  update(id: string, report: any):any{
+    let foundReport=this.data.find(item=>item.id==id)
+    foundReport.name=report.name
+    foundReport.type=report.type
+    
+    return foundReport
+    
+  }
+  create(report: any) {
+  this.data.push(report)
+  return report
+  }
+  find(id: string):any {
+    return this.data.find(item=>item.id==id)
+  }
+  findAll(): any {
+    return this.data
+    
+  }
+  private readonly data=[
+    {
+      id:"1",
+      name: "report1",
+      type:'kpi'
+    },
+    {
+      id:"2",
+      name: "report2",
+      type: ' growth'
+    }
+  ]
+  
 }
